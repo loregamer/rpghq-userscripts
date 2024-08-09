e5342db952ec977f01673f2b1e76a456a6a69e9a
loregamer <162062830+loregamer@users.noreply.github.com>
2024-08-09 16:25:44 -0700

    hide bloomery button except for me

--- BBCode.user.js
+++ BBCode.user.js
@@ -734,13 +734,22 @@ SOFTWARE.
     const smileyBox = document.getElementById("smiley-box");
     if (smileyBox) {
       const bbcodeStatus = smileyBox.querySelector(".bbcode-status");
+      const usernameElement = document.querySelector("span.username");
+      const isLoregamer =
+        usernameElement && usernameElement.textContent.trim() === "loregamer";
+
       if (bbcodeStatus) {
         bbcodeStatus.innerHTML = `
             <hr>
           <button type="button" class="button button-secondary custom-button" id="insert-mod-template">Insert Mod Template</button>
           <button type="button" class="button button-secondary custom-button" id="insert-table">Insert Table</button>
-          <button type="button" class="button button-secondary custom-button" id="ping-bloomery">Ping Bloomery</button>
         `;
+
+        if (isLoregamer) {
+          bbcodeStatus.innerHTML += `
+            <button type="button" class="button button-secondary custom-button" id="ping-bloomery">Ping Bloomery</button>
+          `;
+        }
 
         document
           .getElementById("insert-mod-template")
