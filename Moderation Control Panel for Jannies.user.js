075aaacde71973ba84a6dbadbf7cb7652498d234
loregamer <162062830+loregamer@users.noreply.github.com>
2024-08-09 22:00:16 -0700

    MCP safari compatible

--- Moderation Control Panel for Jannies.user.js
+++ Moderation Control Panel for Jannies.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RPGHQ Moderator Control Panel Enhancer
 // @namespace    https://rpghq.org/
-// @version      3.1.1
+// @version      3.1.2
 // @description  Enhance the look of posts in the moderator control panel to match the forum posts, including profile pictures, fixing post width, adding a fade effect for long posts, and adding a "Show More" button
 // @author       loregamer
 // @match        https://rpghq.org/forums/mcp.php?*mode=topic_view*
@@ -1123,7 +1123,7 @@ SOFTWARE.
     });
   }
 
-  window.addEventListener("load", function () {
+  function init() {
     modifyPostStructure();
     loadSelectedPosts();
     clickExpandView();
@@ -1132,5 +1132,17 @@ SOFTWARE.
     makeDisplayActionsFloat();
     handleMarkAllClicks();
     observeDocumentChanges();
-  });
+  }
+
+  // Run the init function when the page loads
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    // If the document is already ready, execute the function immediately
+    init();
+  } else {
+    // Otherwise, wait for the DOM to be fully loaded
+    window.addEventListener("load", init);
+  }
 })();
