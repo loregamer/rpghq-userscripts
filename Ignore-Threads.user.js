cac60bdf315bcdaa58a3aa6d22ce6fc9e361615d
loregamer <162062830+loregamer@users.noreply.github.com>
2024-08-09 19:07:32 -0700

    Update Ignore-Threads.user.js

--- Ignore-Threads.user.js
+++ Ignore-Threads.user.js
@@ -8,7 +8,9 @@
 // @grant        GM_getValue
 // @grant        GM_registerMenuCommand
 // @license      MIT
-// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABmUExURQCqAQCqAACwDHKocgCrAipPBRggKidQBjgvEkIzDDkvEihQBjkuEjouESpQBTwuEUAtEUMtEEcsEEosEA9fAUsrEEssEEUsEEEtET0uESIkIipOBUItESxOBSUjIitOBS8qHAAAAGMEP4sAAAAidFJOU////////////////////////////////////////////wAN0MNxAAAACXBIWXMAAA7CAAAOwgEVKEqAAAABJklEQVRIS92V6U7DMBCEy+GFBChXucrp939JNju701CxYCNVqphfnsl8iew4zqJ26l8Ai4NJ7mo1d4jx0VchRH8LOMbYiyGE3wJuvRhC6EARgYc9kVOUhhEaYFPgTM7RGAUaYVOgyAUarcBSLtFQADlt9oQrNpDTZsA1G8hpM+CGDeS0GXDLBnJaB1al4AJskTs2kNM6sKU/APdsNAINczCtYJtWyVTCPrDRCHS/uEc2kNNmQOdeWspTNNbIw6YAt/f0JewAKL3A82YO/nFCPwBcpVbgBY0hDhgoBV7lTcfeCukZtc6Ad7NeDOntVf2AXtBt/RGAam7n56QCmkyAkRtgbm3oC9UN6KQ1afoDAVDpeE8AlS2XvTgPfpPNftLOAGrvgFo/AYmjmfleeiNOAAAAAElFTkSuQmCC
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABUUExURfxKZ/9KZutQcjeM5/tLaP5KZokNEhggKnoQFYEPExgfKYYOEhkfKYgOEhsfKYgNEh8eKCIeJyYdJikdJqYJDCocJiodJiQdJyAeKBwfKToaIgAAAKuw7XoAAAAcdFJOU////////////////////////////////////wAXsuLXAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABEUlEQVRIS92S3VLCMBBG8YcsohhARDHv/55uczZbYBra6DjT8bvo7Lc95yJtFqkx/0JY3HWxllJu98wPl2EJfyU8MhtYwnJQWDIbWMLShCBCp65EgKSEWhWeZA1h+KjwLC8Qho8KG3mFUJS912EhytYJ9l6HhSA7J9h7rQl7J9h7rQlvTrD3asIhBF5Qg7w7wd6rCVf5gXB0YqIw4Qw5B+qkr5QTSv1wYpIQW39clE8n2HutCY13aSMnJ9h7rQn99dbnHwixXejPwEBuCP1XYiA3hP7HMZCqEOSks1ElSleFmKuBJSYsM9Eg6Au91l9F0JxXIBd00wlsM9DlvDL/WhgNgkbnmQgaDqOZj+CZnZDSN2ZJgWZx++q1AAAAAElFTkSuQmCC
+// @updateURL    https://github.com/loregamer/rpghq-userscripts/raw/main/Ignore-Threads.user.js
+// @downloadURL  https://github.com/loregamer/rpghq-userscripts/raw/main/Ignore-Threads.user.js
 // ==/UserScript==
 
 /*
