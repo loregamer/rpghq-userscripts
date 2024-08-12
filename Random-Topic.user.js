c31860072b81d4efacd72bccf546d05a50b20f5c
loregamer <162062830+loregamer@users.noreply.github.com>
2024-08-12 12:49:26 -0700

    Update Random-Topic.user.js

--- Random-Topic.user.js
+++ Random-Topic.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
-// @name         RPGHQ Random Topic (Improved)
+// @name         RPGHQ - Random Topic Button
 // @namespace    http://tampermonkey.net/
 // @version      1.0.1
 // @description  Adds a Random Topic button to RPGHQ that ensures the topic exists
 // @match        https://rpghq.org/forums/*
 // @grant        GM_xmlhttpRequest
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABUUExURfxKZ/9KZutQcjeM5/tLaP5KZokNEhggKnoQFYEPExgfKYYOEhkfKYgOEhsfKYgNEh8eKCIeJyYdJikdJqYJDCocJiodJiQdJyAeKBwfKToaIgAAAKuw7XoAAAAcdFJOU////////////////////////////////////wAXsuLXAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABEUlEQVRIS92S3VLCMBBG8YcsohhARDHv/55uczZbYBra6DjT8bvo7Lc95yJtFqkx/0JY3HWxllJu98wPl2EJfyU8MhtYwnJQWDIbWMLShCBCp65EgKSEWhWeZA1h+KjwLC8Qho8KG3mFUJS912EhytYJ9l6HhSA7J9h7rQl7J9h7rQlvTrD3asIhBF5Qg7w7wd6rCVf5gXB0YqIw4Qw5B+qkr5QTSv1wYpIQW39clE8n2HutCY13aSMnJ9h7rQn99dbnHwixXejPwEBuCP1XYiA3hP7HMZCqEOSks1ElSleFmKuBJSYsM9Eg6Au91l9F0JxXIBd00wlsM9DlvDL/WhgNgkbnmQgaDqOZj+CZnZDSN2ZJgWZx++q1AAAAAElFTkSuQmCC
+// @updateURL    https://github.com/loregamer/rpghq-userscripts/raw/main/Random-Topic.user.js
+// @downloadURL  https://github.com/loregamer/rpghq-userscripts/raw/main/Random-Topic.user.js
 // ==/UserScript==
 
 (function () {
