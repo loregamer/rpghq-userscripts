5a51f3bc332a0b3115a70af53a5d376e85df9000
loregamer <162062830+loregamer@users.noreply.github.com>
2024-09-06 16:05:49 -0700

    Create Number-Commas.user.js

---
+++ Number-Commas.user.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         RPGHQ - Thousands Comma Formatter
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Add pin/unpin buttons to threads on rpghq.org and display pinned threads at the top of the board index
+// @match        https://rpghq.org/forums/*
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABUUExURfxKZ/9KZutQcjeM5/tLaP5KZokNEhggKnoQFYEPExgfKYYOEhkfKYgOEhsfKYgNEh8eKCIeJyYdJikdJqYJDCocJiodJiQdJyAeKBwfKToaIgAAAKuw7XoAAAAcdFJOU////////////////////////////////////wAXsuLXAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABEUlEQVRIS92S3VLCMBBG8YcsohhARDHv/55uczZbYBra6DjT8bvo7Lc95yJtFqkx/0JY3HWxllJu98wPl2EJfyU8MhtYwnJQWDIbWMLShCBCp65EgKSEWhWeZA1h+KjwLC8Qho8KG3mFUJS912EhytYJ9l6HhSA7J9h7rQl7J9h7rQlvTrD3asIhBF5Qg7w7wd6rCVf5gXB0YqIw4Qw5B+qkr5QTSv1wYpIQW39clE8n2HutCY13aSMnJ9h7rQn99dbnHwixXejPwEBuCP1XYiA3hP7HMZCqEOSks1ElSleFmKuBJSYsM9Eg6Au91l9F0JxXIBd00wlsM9DlvDL/WhgNgkbnmQgaDqOZj+CZnZDSN2ZJgWZx++q1AAAAAElFTkSuQmCC
+// @updateURL    https://github.com/loregamer/rpghq-userscripts/raw/main/Number-Commas.user.js
+// @downloadURL  https://github.com/loregamer/rpghq-userscripts/raw/main/Number-Commas.user.js
+// @license      MIT
+// ==/UserScript==
+
+/*
+MIT License
+
+Copyright (c) 2024 loregamer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+(function() {
+    'use strict';
+
+    // Regular expression to match numbers with 5 or more digits
+    const numberRegex = /\b\d{5,}\b/g;
+
+    // Function to format a number with commas
+    function formatNumberWithCommas(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    // Function to process and format all text nodes in the document
+    function processTextNodes() {
+        const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+        let node;
+        while (node = walker.nextNode()) {
+            if (numberRegex.test(node.nodeValue)) {
+                node.nodeValue = node.nodeValue.replace(numberRegex, function(match) {
+                    return formatNumberWithCommas(match);
+                });
+            }
+        }
+    }
+
+    // Run the function to format numbers when the page loads
+    processTextNodes();
+
+    // Optionally, you can add a mutation observer to handle dynamically added content
+    const observer = new MutationObserver(processTextNodes);
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
