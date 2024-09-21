0f27c21d15a16d310b9b136edcb29c01b15a8d40
loregamer <162062830+loregamer@users.noreply.github.com>
2024-09-20 21:15:58 -0700

    Update VariousUI.user.js

--- VariousUI.user.js
+++ VariousUI.user.js
@@ -1,27 +1,56 @@
 // ==UserScript==
-// @name         RPGHQ UI Tweaks
+// @name         RPGHQ - Various UI Tweaks
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      1.0
 // @description  Various UI improvements for rpghq.org
-// @match        https://rpghq.org/forums/*
-// @grant        none
+// @match        https://rpghq.org/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @license      MIT
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABUUExURfxKZ/9KZutQcjeM5/tLaP5KZokNEhggKnoQFYEPExgfKYYOEhkfKYgOEhsfKYgNEh8eKCIeJyYdJikdJqYJDCocJiodJiQdJyAeKBwfKToaIgAAAKuw7XoAAAAcdFJOU////////////////////////////////////wAXsuLXAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABEUlEQVRIS92S3VLCMBBG8YcsohhARDHv/55uczZbYBra6DjT8bvo7Lc95yJtFqkx/0JY3HWxllJu98wPl2EJfyU8MhtYwnJQWDIbWMLShCBCp65EgKSEWhWeZA1h+KjwLC8Qho8KG3mFUJS912EhytYJ9l6HhSA7J9h7rQl7J9h7rQlvTrD3asIhBF5Qg7w7wd6rCVf5gXB0YqIw4Qw5B+qkr5QTSv1wYpIQW39clE8n2HutCY13aSMnJ9h7rQn99dbnHwixXejPwEBuCP1XYiA3hP7HMZCqEOSks1ElSleFmKuBJSYsM9Eg6Au91l9F0JxXIBd00wlsM9DlvDL/WhgNgkbnmQgaDqOZj+CZnZDSN2ZJgWZx++q1AAAAAElFTkSuQmCC
+// @updateURL    https://github.com/loregamer/rpghq-userscripts/raw/main/VariousUI.user.js
+// @downloadURL  https://github.com/loregamer/rpghq-userscripts/raw/main/VariousUI.user.js
 // ==/UserScript==
+
+/*
+MIT License
+
+Copyright (c) 2024 loregamer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 (function () {
   "use strict";
 
   const settings = {
-    betterQuoteBoxes: false,
-    betterFileLinks: false,
+    betterQuoteBoxes: true,
+    betterFileLinks: true,
   };
 
   const utils = {
     saveSettings() {
-      localStorage.setItem("rpghqUITweaks", JSON.stringify(settings));
+      GM_setValue("rpghqUITweaks", JSON.stringify(settings));
     },
 
     loadSettings() {
-      const savedSettings = localStorage.getItem("rpghqUITweaks");
+      const savedSettings = GM_getValue("rpghqUITweaks");
       if (savedSettings) {
         Object.assign(settings, JSON.parse(savedSettings));
       }
