1999014940e22ddffb87dc528376d121142b2882
loregamer <162062830+loregamer@users.noreply.github.com>
2025-01-20 13:29:01 -0800

    Update Ghost.user.js

--- Ghost.user.js
+++ Ghost.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ghost Users
 // @namespace    http://tampermonkey.net/
-// @version      1.5.2
+// @version      2.0
 // @description  Add Ghost User button to profiles, hide content from ghosted users, and replace avatars
 // @author       You
 // @match        https://rpghq.org/*/*
