e093c2432a9e2f66f9dd45a905922ae75002c4c2
loregamer <162062830+loregamer@users.noreply.github.com>
2025-02-02 17:55:41 -0800

    Placeholder layout

--- Board-Index-Test.user.js
+++ Board-Index-Test.user.js
@@ -19,11 +19,106 @@
         margin-top: 20px;
     `;
 
-  // Get the main content and recent topics sections
+  // Get the main content and create recent topics box
   const pageBody = document.querySelector("#page-body");
-  const recentTopics = document.querySelector("#recenttopicsbottom");
+  if (!pageBody) return;
 
-  if (!pageBody || !recentTopics) return;
+  // Create the recent topics box
+  const recentTopicsBox = document.createElement("div");
+  recentTopicsBox.id = "recent-topics-box";
+  recentTopicsBox.className = "forabg";
+
+  // Sample topics data
+  const sampleTopics = [
+    {
+      author: "Classix",
+      title: "What are you listening to right now?",
+      forum: "Other » HQ's Theater",
+      lastPost: {
+        author: "Classix",
+        time: "1 minute ago",
+        avatar: "./download/file.php?avatar=3607_1737756902.png",
+      },
+    },
+    {
+      author: "Val the Moofia Boss",
+      title: "What can be done to revive the MMO genre?",
+      forum: "Gaming » Online RPGs",
+      lastPost: {
+        author: "Tweed",
+        time: "7 minutes ago",
+        avatar: "./download/file.php?avatar=68_1718243351.png",
+        color: "#0080FF",
+      },
+    },
+  ];
+
+  // Create the HTML structure
+  recentTopicsBox.innerHTML = `
+    <div class="inner">
+      <ul class="topiclist">
+        <li class="header">
+          <dl class="row-item">
+            <dt></dt>
+            <dd class="list-inner">&nbsp;Recent Topics</dd>
+          </dl>
+        </li>
+      </ul>
+    </div>
+    <div id="recent-topics" class="inner collapsible">
+      <ul>
+        ${sampleTopics
+          .map(
+            (topic, index) => `
+          <li class="author-name-${topic.author.replace(/\s+/g, "-")} row bg${
+              (index % 2) + 1
+            }">
+            <dl class="row-item topic_read">
+              <dt title="No unread posts">
+                <div class="list-inner">
+                  <a href="#" class="topictitle">${topic.title}</a>
+                  <div class="forum-links">
+                    <a class="forum-link" href="#">${topic.forum}</a>
+                  </div>
+                </div>
+              </dt>
+              <dd class="lastpost">
+                <span>
+                  by <div class="mas-wrap">
+                    <div class="mas-avatar" style="width: 20px; height: 20px;">
+                      <img class="avatar" src="${
+                        topic.lastPost.avatar
+                      }" width="128" height="128" alt="User avatar">
+                    </div>
+                    <div class="mas-username">
+                      <a href="#" ${
+                        topic.lastPost.color
+                          ? `style="color: ${topic.lastPost.color};"`
+                          : ""
+                      } class="username${
+              topic.lastPost.color ? "-coloured" : ""
+            }">${topic.lastPost.author}</a>
+                    </div>
+                  </div>
+                  <br>
+                  ${topic.lastPost.time}
+                  <a href="#" title="Go to last post">
+                    <i class="icon fa-external-link-square fa-fw icon-lightgrey icon-md" aria-hidden="true"></i>
+                    <span class="sr-only"></span>
+                  </a>
+                </span>
+              </dd>
+              <dd class="status-icons">
+                <br>
+              </dd>
+            </dl>
+          </li>
+        `
+          )
+          .join("")}
+      </ul>
+    </div>
+  `;
 
   // Style the main content area
   const mainContent = document.createElement("div");
@@ -33,7 +128,7 @@
     `;
 
   // Style the recent topics section
-  recentTopics.style.cssText = `
+  recentTopicsBox.style.cssText = `
         width: 300px;
         flex-shrink: 0;
     `;
@@ -41,14 +136,12 @@
   // Move elements to their new containers
   const elements = Array.from(pageBody.children);
   elements.forEach((el) => {
-    if (el !== recentTopics) {
-      mainContent.appendChild(el);
-    }
+    mainContent.appendChild(el);
   });
 
   // Assemble the new layout
   wrapper.appendChild(mainContent);
-  wrapper.appendChild(recentTopics);
+  wrapper.appendChild(recentTopicsBox);
   pageBody.appendChild(wrapper);
 
   // Adjust responsive styles
@@ -58,7 +151,7 @@
             #page-body > div {
                 flex-direction: column;
             }
-            #recenttopicsbottom {
+            #recent-topics-box {
                 width: 100%;
             }
         }
