0e82ed7a1df2135fa99515cbe34b3082a51674b5
loregamer <162062830+loregamer@users.noreply.github.com>
2025-02-09 16:42:14 -0800

    Update Ignore-Threads.user.js

--- Ignore-Threads.user.js
+++ Ignore-Threads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RPGHQ Thread Ignorer
 // @namespace    http://tampermonkey.net/
-// @version      2.2
+// @version      2.2.1
 // @description  Add ignore/unignore button to threads on rpghq.org and hide ignored threads
 // @match        https://rpghq.org/forums/*
 // @grant        GM_setValue
