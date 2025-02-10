c1751e04e8d8d0085d8fc83ad7088e3e6871ea81
loregamer <162062830+loregamer@users.noreply.github.com>
2025-02-09 16:32:53 -0800

    Update Ghost.user.js

--- Ghost.user.js
+++ Ghost.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ghost Users
 // @namespace    http://tampermonkey.net/
-// @version      4.8.1
+// @version      5.0
 // @description  Hides content from ghosted users + optional avatar replacement, plus quote→blockquote formatting in previews, now with a single spinner per container
 // @author       You
 // @match        https://rpghq.org/*/*
