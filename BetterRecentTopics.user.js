5d9b69ae804982543832e02171508e33a39846cf
loregamer <162062830+loregamer@users.noreply.github.com>
2025-02-17 16:11:26 -0800

    Update BetterRecentTopics.user.js

--- BetterRecentTopics.user.js
+++ BetterRecentTopics.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
-// @name         RPGHQ Recent Topics: No Ellipses + Smaller Parentheses + Post-Dash
-// @namespace    rpghqEnhancer
-// @version      1.0
-// @description  Removes ellipsification on topic titles, makes parentheses smaller, styles version numbers, and unbolds game names
+// @name         RPGHQ - Better Recent Topics
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Removes ellipses/truncation in titles, makes parentheses smaller, styles version numbers, and unbolds game names
 // @match        https://rpghq.org/forums/*
 // @grant        none
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABUUExURfxKZ/9KZutQcjeM5/tLaP5KZokNEhggKnoQFYEPExgfKYYOEhkfKYgOEhsfKYgNEh8eKCIeJyYdJikdJqYJDCocJiodJiQdJyAeKBwfKToaIgAAAKuw7XoAAAAcdFJOU////////////////////////////////////wAXsuLXAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABEUlEQVRIS92S3VLCMBBG8YcsohhARDHv/55uczZbYBra6DjT8bvo7Lc95yJtFqkx/0JY3HWxllJu98wPl2EJfyU8MhtYwnJQWDIbWMLShCBCp65EgKSEWhWeZA1h+KjwLC8Qho8KG3mFUJS912EhytYJ9l6HhSA7J9h7rQl7J9h7rQlvTrD3asIhBF5Qg7w7wd6rCVf5gXB0YqIw4Qw5B+qkr5QTSv1wYpIQW39clE8n2HutCY13aSMnJ9h7rQn99dbnHwixXejPwEBuCP1XYiA3hP7HMZCqEOSks1ElSleFmKuBJSYsM9Eg6Au91l9F0JxXIBd00wlsM9DlvDL/WhgNgkbnmQgaDqOZj+CZnZDSN2ZJgWZx++q1AAAAAElFTkSuQmCC
+// @updateURL    https://github.com/loregamer/rpghq-userscripts/raw/main/BetterRecentTopics.user.js
+// @downloadURL  https://github.com/loregamer/rpghq-userscripts/raw/main/BetterRecentTopics.user.js
 // ==/UserScript==
 
 (function () {
