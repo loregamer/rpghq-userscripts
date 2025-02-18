43faeae46433f2b539882756c89283a6ceb35f85
loregamer <162062830+loregamer@users.noreply.github.com>
2025-02-17 19:53:15 -0800

    Update Notifications.user.js

--- Notifications.user.js
+++ Notifications.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RPGHQ Notifications Customization
 // @namespace    http://tampermonkey.net/
-// @version      4.2
+// @version      4.2.1
 // @description  Customize RPGHQ notifications display
 // @author       LOREGAMER
 // @match        https://rpghq.org/*/*
