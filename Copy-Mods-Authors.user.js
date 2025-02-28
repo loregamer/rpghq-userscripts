d1ca443ad77e4532af601f3631959a42de6dd121
loregamer <162062830+loregamer@users.noreply.github.com>
2025-02-28 10:40:05 -0800

    Update Copy-Mods-Authors.user.js

--- Copy-Mods-Authors.user.js
+++ Copy-Mods-Authors.user.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name         RPGHQ - Copy Mods and Authors
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.1
 // @description  Adds "Copy Mods" and "Copy Authors" buttons to forum threads
 // @match        https://rpghq.org/forums/viewtopic.php?t=3511*
+// @match        https://rpghq.org/forums/viewtopic.php*submissions-for-nexus*
 // @grant        GM_addStyle
 // @license      MIT
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABUUExURfxKZ/9KZutQcjeM5/tLaP5KZokNEhggKnoQFYEPExgfKYYOEhkfKYgOEhsfKYgNEh8eKCIeJyYdJikdJqYJDCocJiodJiQdJyAeKBwfKToaIgAAAKuw7XoAAAAcdFJOU////////////////////////////////////wAXsuLXAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABEUlEQVRIS92S3VLCMBBG8YcsohhARDHv/55uczZbYBra6DjT8bvo7Lc95yJtFqkx/0JY3HWxllJu98wPl2EJfyU8MhtYwnJQWDIbWMLShCBCp65EgKSEWhWeZA1h+KjwLC8Qho8KG3mFUJS912EhytYJ9l6HhSA7J9h7rQl7J9h7rQlvTrD3asIhBF5Qg7w7wd6rCVf5gXB0YqIw4Qw5B+qkr5QTSv1wYpIQW39clE8n2HutCY13aSMnJ9h7rQn99dbnHwixXejPwEBuCP1XYiA3hP7HMZCqEOSks1ElSleFmKuBJSYsM9Eg6Au91l9F0JxXIBd00wlsM9DlvDL/WhgNgkbnmQgaDqOZj+CZnZDSN2ZJgWZx++q1AAAAAElFTkSuQmCC
+// @updateURL    https://github.com/loregamer/rpghq-userscripts/raw/main/Copy-Mods-Authors.user.js
+// @downloadURL  https://github.com/loregamer/rpghq-userscripts/raw/main/Copy-Mods-Authors.user.js
 // ==/UserScript==
 
 /*
