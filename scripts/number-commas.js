54d9c72f2df3c04f4c54b84a2ba60d5e98e4d483
loregamer <162062830+loregamer@users.noreply.github.com>
2025-03-20 18:26:56 -0700

    Deletions

--- scripts/number-commas.js
+++ scripts/number-commas.js
@@ -1,47 +1,13 @@
-// ==UserScript==
-// @name         RPGHQ - Thousands Comma Formatter
-// @namespace    http://tampermonkey.net/
-// @version      2.1.2
-// @description  Add commas to numbers
-// @match        https://rpghq.org/forums/*
-// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABUUExURfxKZ/9KZutQcjeM5/tLaP5KZokNEhggKnoQFYEPExgfKYYOEhkfKYgOEhsfKYgNEh8eKCIeJyYdJikdJqYJDCocJiodJiQdJyAeKBwfKToaIgAAAKuw7XoAAAAcdFJOU////////////////////////////////////wAXsuLXAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABEUlEQVRIS92S3VLCMBBG8YcsohhARDHv/55uczZbYBra6DjT8bvo7Lc95yJtFqkx/0JY3HWxllJu98wPl2EJfyU8MhtYwnJQWDIbWMLShCBCp65EgKSEWhWeZA1h+KjwLC8Qho8KG3mFUJS912EhytYJ9l6HhSA7J9h7rQl7J9h7rQlvTrD3asIhBF5Qg7w7wd6rCVf5gXB0YqIw4Qw5B+qkr5QTSv1wYpIQW39clE8n2HutCY13aSMnJ9h7rQn99dbnHwixXejPwEBuCP1XYiA3hP7HMZCqEOSks1ElSleFmKuBJSYsM9Eg6Au91l9F0JxXIBd00wlsM9DlvDL/WhgNgkbnmQgaDqOZj+CZnZDSN2ZJgWZx++q1AAAAAElFTkSuQmCC
-// @updateURL    https://github.com/loregamer/rpghq-userscripts/raw/main/Number-Commas.user.js
-// @downloadURL  https://github.com/loregamer/rpghq-userscripts/raw/main/Number-Commas.user.js
-// @license      MIT
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_registerMenuCommand
-// @grant        GM_unregisterMenuCommand
-// ==/UserScript==
-
-/*
-MIT License
-
-Copyright (c) 2024 loregamer
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+// Number Commas Script
+// Version: 2.1.2
+// Description: Add commas to numbers
+// Author: loregamer
 
 (function () {
   "use strict";
 
-  const formatFourDigits = GM_getValue("formatFourDigits", false);
+  // Get settings from the script manager
+  const formatFourDigits = scriptSettings.formatFourDigits || false;
 
   const numberRegex = formatFourDigits ? /\b\d{4,}\b/g : /\b\d{5,}\b/g;
 
@@ -176,23 +142,6 @@ SOFTWARE.
       });
     });
   }
-
-  function toggleFourDigitFormatting() {
-    const newValue = !GM_getValue("formatFourDigits", false);
-    GM_setValue("formatFourDigits", newValue);
-    updateMenuLabel(newValue);
-    location.reload();
-  }
-
-  function updateMenuLabel(formatFourDigits) {
-    const label = formatFourDigits
-      ? "Disable 4-digit formatting"
-      : "Enable 4-digit formatting";
-    GM_unregisterMenuCommand("Toggle 4-digit formatting");
-    GM_registerMenuCommand(label, toggleFourDigitFormatting);
-  }
-
-  updateMenuLabel(formatFourDigits);
 
   // Run initial processing
   processElements();
