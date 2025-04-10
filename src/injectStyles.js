48b84e99382a1bf4f756fea65c7d8758b5481413
loregamer <162062830+loregamer@users.noreply.github.com>
2025-04-09 20:25:05 -0700

    feat: Wrap injected CSS in backticks

--- src/injectStyles.js
+++ src/injectStyles.js
@@ -1,7 +1,7 @@
 import css from './styles.css'; // Import CSS using rollup-plugin-string
 
 // Replace literal \n with actual newlines before injecting
-GM_addStyle(css.replace(/\\n/g, '\n'));
+GM_addStyle(`${css.replace(/\\n/g, '\n')}`);
 console.log(
   "RPGHQ Manager: Styles injected via GM_addStyle from injectStyles.js."
 );
